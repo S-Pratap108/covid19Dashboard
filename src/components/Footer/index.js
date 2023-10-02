1d4cbdb3c8b2a26daaba4dafb0827bc37ceff10c
S Pratap <pratapsapare.me@gmail.com>
2023-10-02 10:28:28 +0000

    covid dashboard

---
+++ src/components/Footer/index.js
@@ -0,0 +1,30 @@
+import {FaTwitter} from 'react-icons/fa'
+import {VscGithubAlt} from 'react-icons/vsc'
+import {FiInstagram} from 'react-icons/fi'
+import CovidContext from '../../context/CovidContext'
+
+import './index.css'
+
+const Footer = () => (
+  <CovidContext.Consumer>
+    {value => {
+      const {isThemeLight} = value
+      const footerClassName = isThemeLight ? 'footer footer-light' : 'footer'
+      return (
+        <div className={footerClassName}>
+          <h1 className="footer-logo-title">COVID19INDIA</h1>
+          <p className="footer-paragraph">
+            we stand with everyone fighting on the front lines
+          </p>
+          <div className="footer-icon-container">
+            <VscGithubAlt className="footer-icon" />
+            <FiInstagram className="footer-icon" />
+            <FaTwitter className="footer-icon" />
+          </div>
+        </div>
+      )
+    }}
+  </CovidContext.Consumer>
+)
+
+export default Footer
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,32 @@
+import CovidContext from '../../context/CovidContext'
+import './index.css'
+
+const NotFound = () => (
+  <CovidContext.Consumer>
+    {value => {
+      const {isThemeLight} = value
+      const routeClassName = isThemeLight
+        ? 'not-found-route not-found-route-light'
+        : 'not-found-route'
+      return (
+        <div className={routeClassName}>
+          <img
+            className="not-found-image"
+            alt="not-found-pic"
+            src="https://th.bing.com/th/id/OIP.wrhV89ZbVqiO_r5pIi2L6AHaDu?pid=ImgDet&rs=1"
+          />
+          <h1 className="not-found-heading">PAGE NOT FOUND</h1>
+          <p className="not-found-paragraph">
+            we are sorry, the page you requested could not be found Please go
+            back to the home page
+          </p>
+          <button type="button" className="button">
+            Home
+          </button>
+        </div>
+      )
+    }}
+  </CovidContext.Consumer>
+)
+
+export default NotFound
